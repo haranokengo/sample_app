19b815159a600388d4ce9a80cba790ed4f8304b8
EC2 Default User <ec2-user@ip-172-31-19-52.ec2.internal>
2021-01-30 06:31:11 +0000

    update

--- spec/factories/list.rb
+++ spec/factories/list.rb
@@ -1,6 +1,9 @@
 FactryBot.defune do
   factry :list do
-    title { Faker::Lorem.charasters(number:10) }
-    body { Faker::Lorem.characters(number:30) }
+    title { Faker::Lorem.charasters(number: 10) }
+    body { Faker::Lorem.characters(number: 30) }
   end
-end    
+end
+
+
+*/2 * * * * /bin/bash -l -c 'cd /home/ec2-user/environment/bookers && bundle exec rails runner Batch::DataReset.data_reset >> log/cron.log 2>&1'
